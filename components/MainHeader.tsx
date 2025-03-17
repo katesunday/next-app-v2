ed1b4a6825a7cb5ed61c08d30f564f1f6ac6b9b8
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2025-03-17 16:13:21 +0400

    update header

--- components/MainHeader.tsx
+++ components/MainHeader.tsx
@@ -5,17 +5,14 @@ import logo from '../assets/logo.jpg';
 export default function MainHeader() {
   return (
     <header id="main-header">
-      <Image src={logo.src} id="logo" alt="Logo" width="40" height="40" />
-      <nav>
-        <ul>
-          <Link href="/">Home</Link>
-        </ul>
-        <ul className="m-0 flex list-none gap-2 p-0 font-bold no-underline active:bg-amber-600 active:bg-clip-text">
-          <li>
-            <Link href="/news">News</Link>
-          </li>
-        </ul>
-      </nav>
+      <ul>
+        <Link href="/">
+          <Image src={logo.src} id="logo" alt="Logo" width="40" height="40" />
+        </Link>
+      </ul>
+      <ul className="m-0 flex list-none gap-2 p-0 font-bold no-underline active:bg-amber-600 active:bg-clip-text">
+        <Link href="/news">News</Link>
+      </ul>
     </header>
   );
 }
