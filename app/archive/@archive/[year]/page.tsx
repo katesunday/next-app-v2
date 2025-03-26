825fd264e1a127857074648c01fb3287e1e2d60f
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2025-03-26 13:29:40 +0400

    refactor parallel routes

--- app/archive/@archive/[year]/page.tsx
+++ app/archive/@archive/[year]/page.tsx
@@ -4,5 +4,10 @@ import { getNewsForYear } from '../../../../lib/news';
 export default function FilteredNewsPage({ params }) {
   const newsYear = params.year;
   const news = getNewsForYear(newsYear);
-  return <NewsList news={news} />;
+  return (
+    <>
+      <p>{params.year}</p>
+      <NewsList news={news} />
+    </>
+  );
 }
