90406516e917ad0e1fe0b7609580fa295ff5290d
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2025-03-26 14:38:01 +0400

    news for months

--- app/archive/@archive/[[...filter]]/page.tsx
+++ app/archive/@archive/[[...filter]]/page.tsx
@@ -1,5 +1,10 @@
 import NewsList from '../../../../components/NewsList';
-import { getAvailableNewsYears, getNewsForYear } from '../../../../lib/news';
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from '../../../../lib/news';
 import Link from 'next/link';
 import { NewsType } from '../../../../dummy-news';
 
@@ -7,10 +12,16 @@ export default function FilteredNewsPage({ params }) {
   const filter = params.filter;
   const selectedYear = filter?.[0];
   const selectedMonth = filter?.[1];
+  let links = getAvailableNewsYears();
 
   let news: NewsType[];
   if (selectedYear && !selectedMonth) {
     news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = getAvailableNewsMonths(selectedYear);
   }
   let newsContent = <p>No news found for the selected period. </p>;
 
@@ -18,24 +29,28 @@ export default function FilteredNewsPage({ params }) {
     newsContent = <NewsList news={news} />;
   }
 
-  const links = getAvailableNewsYears();
   return (
     <>
       <header id="archive-header">
         <nav>
           <ul>
-            {links.map((link) => (
-              <li
-                key={link}
-                style={
-                  selectedYear == link
-                    ? { textDecoration: 'underline' }
-                    : undefined
-                }
-              >
-                <Link href={`/archive/${link}`}>{link}</Link>
-              </li>
-            ))}
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+              return (
+                <li
+                  key={link}
+                  style={
+                    selectedYear == link || selectedMonth == link
+                      ? { textDecoration: 'underline' }
+                      : undefined
+                  }
+                >
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
           </ul>
         </nav>
       </header>
