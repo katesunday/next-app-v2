7ac35e582e2f10b6d3845fc00f2c7744745136a4
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2025-04-01 17:29:08 +0400

    fix import

--- app/news/[slug]/@modal/(.)image/page.tsx
+++ app/news/[slug]/@modal/(.)image/page.tsx
@@ -1,5 +1,5 @@
-import { DUMMY_NEWS } from '../../../../dummy-news';
 import { notFound } from 'next/navigation';
+import { DUMMY_NEWS } from '../../../../../dummy-news';
 
 export default function InterceptedImagePage({
   params,
